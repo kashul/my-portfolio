3050490c51d959056abf95f5be7e552bd0d9afbf
kashul <kasulkenuka@gmail.com>
2024-11-07 02:21:03 +0530

    update

--- src/app/projects/@modalview/(..)view/[viewtype]/[id]/page.tsx
+++ src/app/projects/@modalview/(..)view/[viewtype]/[id]/page.tsx
@@ -37,7 +37,7 @@ const [open, setOpen] = useState(true)
 if(project){
     return (
       <>
-       <Dialog   className="relative z-10 " open={open} onClose={setOpen}>
+       <Dialog    className="relative z-10 " open={open} onClose={setOpen}>
        {/* <DialogBackdrop
         transition
         className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
@@ -45,7 +45,7 @@ if(project){
       
    
       <div   className="relative   z-10 mx-auto md:py-10    w-screen md:w-11/12 h-dvh  ">
-      <div  data-autofocus className=" w-full h-full overflow-y-auto view-background rounded-lg ">
+      <div   className=" w-full h-full overflow-y-auto view-background rounded-lg ">
       <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
             
             <button
@@ -105,6 +105,19 @@ if(project){
       
      <div className="relative   z-10 mx-auto md:py-10   w-screen md:w-11/12 h-dvh  ">
      <div className=" w-full h-full overflow-y-auto view-background rounded-lg ">
+     <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            
+            <button
+              type="button"
+              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              onClick={() => {
+                router.back()
+              }}
+              data-autofocus="true"
+            >
+              X
+            </button>
+          </div>
      <div className="flex  pt-10">
         
      <Activity activity={activity}></Activity>
